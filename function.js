311b8450fe7c6927fd007dc87e8f23174496692a
Grunii <karl.elmgren@gmail.com>
2021-11-02 12:46:45 +0100

    added timestamp

--- function.js
+++ function.js
@@ -6,5 +6,25 @@ function submitform(){
     const epost = document.forms["guestbook"]["epost"].value;
     const telefon = document.forms["guestbook"]["telefon"].value;
     const meddelande = document.forms["guestbook"]["meddelande"].value;
+
+    const date =date.now();
+    console.log(date);
+    const today = new date(date);
+    console.log(date);
+
+    var message = document.getElementById(message).innerHTMLM;
+
+    message +=`
+    ${namn}, (${epost})
+    <br>
+    ${telefon}
+    <br>
+    ${meddelande}
+    <br>
+    ${today.toUTCstring()}
+    <hr>
+    `;
+
+
     return false;
 }
